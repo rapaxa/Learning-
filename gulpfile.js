dc35d1db6f60f1fa7eba064aebe38dee41312f60
PAVLO VOLODCHENKO <rapaxa123@gmail.com>
2022-10-10 23:58:36 +0100

    second

--- gulpfile.js
+++ gulpfile.js
@@ -4,30 +4,42 @@ const sass        = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
-
+gulp.task('html',function (){
+    return gulp.src("src/*.html")
+        .pipe(gulp.dest("dist/"))
+});
+gulp.task('image',function (){
+    return gulp.src("src/img/*.*")
+        .pipe(gulp.dest("dist/img"))
+});
+gulp.task('script',function (){
+    return gulp.src("src/**/*.js")
+        .pipe(gulp.dest("dist/"))
+});
 gulp.task('server', function() {
 
     browserSync({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
 
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("dist/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('styles', function() {
     return gulp.src("src/**/*.+(scss|sass)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(sass())
+        .pipe(rename({suffix: '', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("dist/"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
     gulp.watch("src/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("src/**/*.html", gulp.parallel('html'));
 })
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('default', gulp.parallel('watch',"html","image", 'server',"script", 'styles'));
