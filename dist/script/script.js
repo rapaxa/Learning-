3ef06e53fe4f8543f4422b09072e27caab7a7f4e
PAVLO VOLODCHENKO <rapaxa123@gmail.com>
2022-10-17 15:36:36 +0100

    bootstrap

--- dist/script/script.js
+++ dist/script/script.js
@@ -1,28 +1,2 @@
-const modalButton = document.getElementById('header__btn')
-const modalClose = document.getElementById('close__btn')
-function modalHeader() {
-    const modal = document.querySelector('.promo__modal')
-    const stat = modal.style.display;
-    if(stat === 'flex'){
-        modal.style.display = 'none'
-    }else{
-        modal.style.display = 'flex'
-    }
 
-}
-function modalOpen(name){
-    name.addEventListener('click', (e) => {
-        modalHeader()
-    })
-}
-
-
-modalOpen(modalButton)
-modalOpen(modalClose)
-// modalButton.addEventListener('click', (e) => {
-//     modalHeader()
-// })
-// modalClose.addEventListener('click', (e) => {
-//     modalHeader()
-// })
 
