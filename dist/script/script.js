59fab49c639da6d10a5dbc76643d45b7c8680dd9
Pavel VOlodchenko <rapaxa123@gmail.com>
2022-10-18 14:07:12 +0100

    bootstrap

--- dist/script/script.js
+++ dist/script/script.js
@@ -1,2 +1,28 @@
+window.addEventListener('DOMContentLoaded', () => {
+    function showTab(i){
+        i.classList.add('active')
+    }
+    function tabs() {
+        const tabsParent = document.querySelector('.menu')
+        tabsParent.addEventListener('click', function (event) {
+
+            const tab = document.querySelectorAll('.menu__item')
+            const target = event.target;
+            if (target && target.classList.contains('menu__item')) {
+                tab.forEach((i, index) => {
+                    if (target === i) {
+                        showTab(i)
+                    }else{
+                        i.classList.remove('active')
+                    }
+                })
+            }
+        });
+    }
+
+
+    tabs()
+
+})
 
 
